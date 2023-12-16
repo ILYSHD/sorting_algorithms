dc937bae38a03d138cd06511ee6373047abeac3b
ILYSHD <ilyashaddaoui0@gmail.com>
2023-12-16 14:21:16 -0800

    adding task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - a function
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	if (array == NULL)
+		return;
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
