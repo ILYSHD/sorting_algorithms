256ef6a8d60e80c82edc117146d5487188fa0d43
ILYSHD <ilyashaddaoui0@gmail.com>
2023-12-19 06:01:57 -0800

    adding task 2 script

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - a function to sort
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min, tmp;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
